f2f7e21392865ae918e703ae8debebc9fa09a786
arvindkumarts <arvind.ts@jumeirah.com>
2022-05-15 19:28:33 +0400

    react is fun

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React is fun
         </a>
       </header>
     </div>
